91c78f16dadcd75658b7754b90fc620457e65ced
Vargha Khallokhi <vkhalokhi@gmail.com>
2021-08-25 12:32:28 +0430

    [models] Appending __init__ file for models

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
 from .resnet50 import Resnet50
+from .InceptionResNetV2 import InceptionResnetV2
 
 MODELS = dict(resnet50=Resnet50,
+              inception_resnetv2=InceptionResnetV2
               # other models
               )
 
