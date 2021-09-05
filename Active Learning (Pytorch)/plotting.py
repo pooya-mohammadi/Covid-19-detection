d78755630ee99b83664eb8f3c0766c76e33a86e7
Vargha Khalokhi <vkhalokhi@gmail.com>
2021-09-05 21:49:44 +0430

    Appending plotting function

---
+++ Active Learning (Pytorch)/plotting.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+
+def plot(history):
+    plt.plot(history['accuracy'])
+    plt.plot(history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('query')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('query')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
