94f51a45e78e8577442cecb1390474bd45021ad8
Vargha Khalokhi <vkhalokhi@gmail.com>
2021-09-05 21:49:27 +0430

    Appending hyperparameters setting hp

---
+++ Active Learning (Pytorch)/hp.py
@@ -0,0 +1,17 @@
+"""
+This module contains hyper-parameter modules
+"""
+
+
+def load_hps(dataset_dir, model_name, batch_size, n_epochs, learning_rate, lr_reducer_factor, lr_reducer_patience,
+             img_size, framework, query_strategy, query_size, pool_size, **kwargs) -> dict:
+    hps = dict(
+        dataset_dir=dataset_dir, model_name=model_name, batch_size=batch_size, n_epochs=n_epochs,
+        learning_rate=learning_rate,
+        lr_reducer_factor=lr_reducer_factor,
+        lr_reducer_patience=lr_reducer_patience, img_size=img_size, framework=framework, query_strategy=query_strategy,
+        query_size=query_size, pool_size=pool_size
+    )
+    if kwargs is not None:
+        hps.update(kwargs)
+    return hps
